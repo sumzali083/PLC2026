5db3266357f0d34a1c5426d3ebe9ec9a41debfdd
Summer Ali <alisummer066@icloud.com>
2026-02-26 13:35:34 +0000

    Write code that extracts all items that are less than 20 seconds long

--- lab6-collections/java/PlaylistColls.java
+++ lab6-collections/java/PlaylistColls.java
@@ -103,8 +103,11 @@ public class PlaylistColls {
 
         List<Float> shortItemLengths1 = new ArrayList<>();
         // TASK 6-1(b)...
-
-
+       for (Item item : playlist1) {
+        if (item.length_secs> 20) {
+             shortItemLengths1.add(item.length_secs);
+             }
+            }
 
 
 

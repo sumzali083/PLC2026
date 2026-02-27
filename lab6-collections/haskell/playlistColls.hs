ea7ae068cb9851221033c5374d7ef1f80a3f2c98
Summer Ali <alisummer066@icloud.com>
2026-02-27 12:53:42 +0000

    completed list comprehension in haskell

--- lab6-collections/haskell/playlistColls.hs
+++ lab6-collections/haskell/playlistColls.hs
@@ -54,9 +54,9 @@ playlist2 = [piece2, advert1]
 
 playlists = [playlist1, playlist2] -- a list of lists
 
-adsFromPlaylists = "todo"  -- TASK 6.3-b
+adsFromPlaylists = [item | list <- playlists, item <- list, isAdvert item]
 
-shortItemLenghts1 = "todo" -- TASK 6.3-a
+shortItemLenghts1 = [item_length_secs item | item <- playlist1, item_length_secs item < 20]
 
 main =
     do
